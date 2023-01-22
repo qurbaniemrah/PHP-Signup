606b9c1697d69606d4ef13e06357858fe877db4a
qurbaniemrah <qurbaniemrah@outlook.com>
2023-01-22 12:05:29 +0400

    completed

--- signup1/login.php
+++ signup1/login.php
@@ -1,44 +1,30 @@
 <?php
-$success=0;
-$user=0;
-
-
+$login=0;
+$invalid=0;
 
 if($_SERVER['REQUEST_METHOD']=='POST') {
     include 'connect.php';
     $username=$_POST['username'];
     $password=$_POST['password'];
 
-    // $sql="insert into `registration`(username,password) values('$username','$password')";
-    // $result=mysqli_query($con,$sql);
-
-    // if($result){
-    //     echo "Data inserted successfully";
-    // }else {
-    //     die(mysqli_error($con));
-    // }
-    $sql="select * from `registration` where username='$username'";
+   $sql="select * from `registration` where username='$username' and password='$password'";
     $result=mysqli_query($con,$sql);
 
     if($result) {
       $num=mysqli_num_rows($result);
       if($num>0) {
-        // echo "User alredy exist";
-        $user=1;
+      
+        $login=1;
+        session_start();
+        $_SESSION['username']=$username;
+        header('location:home.php');
 
       }else {
-        $sql="insert into `registration`(username,password) values('$username','$password')";
-        $result=mysqli_query($con,$sql);
-          if($result){
-        // echo "Signup successfull";
-        $success=1;
-        header('location:login.php')
-    }else {
-        die(mysqli_error($con));
+      $invalid=1;
+    }
     }
       }
-    }
-}
+
 
 
 
@@ -61,37 +47,37 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Signup page</title>
+    <title>Login Page</title>
   </head>
   <body>
 <?php
-if($user) {
-  echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Oh no sorry</strong>User alredy exist.
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>';
-}
+  if($login) {
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Success</strong>You are successufly login.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>';
+  }
 
-if($success) {
-  echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>Success</strong>You are successufly signup.
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>';
-}
+?>
 
+<?php
+  if($invalid) {
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Error</strong>Invalid credentials.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>';
+  }
 
 ?>
 
 
-
-
-    <h1 class="text-center">Signup Page</h1>
+    <h1 class="text-center">Login to our website</h1>
 <div class="container mt-5">
-<form action="sign.php" method="post">
+<form action="login.php" method="post">
   <div class="form-group">
     <label for="exampleInputEmail1">Name</label>
     <input type="text" class="form-control"  placeholder="Enter Your Username" name="username">
@@ -102,7 +88,7 @@ if($success) {
     <input type="password" class="form-control"  placeholder="Enter Your Password" name="password">
   </div>
 
-  <button type="submit" class="btn btn-primary w-100">Signup</button>
+  <button type="submit" class="btn btn-primary w-100">Login</button>
 </form>
 </div>
 
